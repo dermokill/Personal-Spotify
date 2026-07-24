58959373ed752b590543e7e33b24454dc9750762
M05703 <saad.boukili@ext.bpifrance.fr>
2026-07-24 15:30:41 +0100

    initial commit

---
+++ src/main/java/com/spotviper/SpotViperApplication.java
@@ -0,0 +1,13 @@
+package com.spotviper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotViperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpotViperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/spotviper/SpotViperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spotviper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotViperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
